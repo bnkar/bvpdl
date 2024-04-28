241f9309124160dcdf75c8bcf1c118cca4f8f9f7
bnkar <134537643+bnkar@users.noreply.github.com>
2024-04-28 22:28:33 +0530

    Update TokenRing.java

--- TokenRing.java
+++ TokenRing.java
@@ -1,59 +1,58 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class TokenRing{
-	/**
-	 * This Java function simulates token passing in a ring network and allows the user to send data
-	 * between nodes.
-	 */
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter Number of Nodes you want in the Ring: ");
-		int n = sc.nextInt();		
-		System.out.println("");
-		System.out.println("Ring Formed is as below: ");
-		
-		for(int i=0; i<n; i++){
-			System.out.print(i + " ");
-		}
-		System.out.println("0");
-		System.out.println("");
-		int choice = 0;
-		
-		do{
-			System.out.println("Enter Sender: ");
-			int sender = sc.nextInt();
-			
-			System.out.println("Enter Receiver: ");
-			int receiver = sc.nextInt();
-			sc.nextLine();
-			
-			System.out.print("Enter data to be send: ");
-			String data = sc.nextLine();
-			System.out.println("");
-			int token = 0;
-			
-			System.out.print("Token Passing: ");
-			
-			for(int i = token; i < receiver; i++){
-				System.out.print(" " + i + " ->");
-			} 
-			
-			System.out.println(" " + receiver);
-			System.out.println("Sender: " + sender + " , Sending Data: " + data);
-			
-			for(int i = sender; i != receiver; i=(i+1)%n){
-				System.out.println("Data: " + data + " , Forwarded By: " + i);
-			}
-			
-			System.out.println("Receiver: " + receiver + " , Received the Data: " + data);
-			
-			token = sender;
-			System.out.println("");
-			System.out.print("Do you want to send data again? If YES enter 1, If NO enter 0: ");
-			choice = sc.nextInt();
-			
-		} while(choice==1);
-		
-		sc.close();
-	}
+class tokenring {
+
+    public static void main(String args[]) throws Throwable {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the num of nodes:");
+        int n = scan.nextInt();
+        int m = n - 1;
+        // Decides the number of nodes forming the ring
+        int token = 0;
+        int ch = 0, flag = 0;
+        for (int i = 0; i <= n; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println("");
+        do{
+            System.out.println("Enter sender:");
+            int s = scan.nextInt();
+            System.out.println("Enter receiver:");
+            int r = scan.nextInt();
+            System.out.println("Enter Data:");
+            int a;
+            a = scan.nextInt();       
+            System.out.print("Token passing:");
+           /* for (int i = token, j = token; (i % n) != s; i++, j = (j + 1) % n) {
+                System.out.print(" " + j + "->");
+            }
+            System.out.println(" " + s);*/
+            for(int i=s; i<=r; i++){
+               System.out.print(" " + i + "->");  
+            }
+            System.out.println(" ");
+
+            System.out.println("Sender " + s + " sending data: " + a);
+            for (int i = s + 1; i != r; i = (i + 1) % n) {
+                System.out.println("data  " + a + " forwarded by " + i);
+            }
+            System.out.println("Receiver " + r + " received data: " + a +"\n");
+            token = s;
+            do{	
+                try {
+                    if( flag == 1)
+			System.out.print("Invalid Input!!...");
+                    System.out.print("Do you want to send again?? enter 1 for Yes and 0 for No : ");
+                    ch = scan.nextInt();
+                    if( ch != 1 && ch != 0 )
+			flag = 1;
+                    else
+			flag = 0;                    
+                } catch (InputMismatchException e){
+                    System.out.println("Invalid Input");
+                }
+            }while( ch != 1 && ch != 0 ); 
+        }while( ch == 1 );            
+    }
 }
